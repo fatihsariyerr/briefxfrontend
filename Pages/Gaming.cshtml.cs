7343241ed856dd3b097059123497d6e219762cf4
fatihsariyerr <fatihsariyerr@gmail.com>
2024-10-06 20:39:39 +0300

    all

---
+++ Pages/Gaming.cshtml.cs
@@ -0,0 +1,169 @@
+﻿using System.Xml;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Npgsql;
+namespace pulseui.Pages;
+
+public class GamingModel : PageModel
+{
+    private readonly ILogger<GamingModel> _logger;
+
+    public GamingModel(ILogger<GamingModel> logger)
+    {
+        _logger = logger;
+    }
+  private string _connectionString;
+  public List<Haber> news { get; set; } = new List<Haber> { };
+  public List<SonDakika> sondakikahaberleri { get; set; } = new List<SonDakika> { };
+  
+  public void OnGet(string search = null)
+  {
+    Sondakika();
+    _connectionString = "User ID=atedevuser1;Password=atedevpostgrepwd1;Server=10.150.0.79;Port=31821;Database=pulse;";
+
+    using (var connection = new NpgsqlConnection(_connectionString))
+    {
+      connection.Open();
+
+      string query = "SELECT * FROM news where category='gaming' ORDER BY publishdate DESC";
+      if (!string.IsNullOrEmpty(search))
+      {
+        query = " SELECT * FROM news where title ILIKE @search ORDER BY publishdate DESC";
+      }
+
+      using (var command = new NpgsqlCommand(query, connection))
+      {
+        if (!string.IsNullOrEmpty(search))
+        {
+          command.Parameters.AddWithValue("@search", "%" + search + "%"); 
+        }
+
+        using (var reader = command.ExecuteReader())
+        {
+          while (reader.Read())
+          {
+            var publishedAt = reader.GetDateTime(reader.GetOrdinal("publishdate")); 
+
+            DateTime now = DateTime.Now;
+            TimeSpan timeDifference = now - publishedAt;
+
+            string timeAgo;
+            if (timeDifference.TotalMinutes < 60)
+            {
+              timeAgo = $"{(int)timeDifference.TotalMinutes} dakika önce";
+            }
+            else if (timeDifference.TotalHours < 24)
+            {
+              timeAgo = $"{(int)timeDifference.TotalHours} saat önce";
+            }
+            else
+            {
+              timeAgo = $"{(int)timeDifference.TotalDays} gün önce";
+            }
+
+
+            var cekilenhaber = new Haber
+            {
+              Title = reader.GetString(reader.GetOrdinal("title")),
+              ImageUrl = reader.IsDBNull(reader.GetOrdinal("image")) ? "/assets/img/briefxlogo.png" : reader.GetString(reader.GetOrdinal("image")),
+              Link = reader.GetString(reader.GetOrdinal("link")),
+              Publisher = reader.GetString(reader.GetOrdinal("publisher")),
+              PublishedAtFormatted = timeAgo 
+            };
+
+            news.Add(cekilenhaber);
+          }
+        }
+      }
+    }
+  }
+
+
+
+  public void Sondakika()
+  {
+    _connectionString = "User ID=atedevuser1;Password=atedevpostgrepwd1;Server=10.150.0.79;Port=31821;Database=pulse;";
+
+    using (var connection = new NpgsqlConnection(_connectionString))
+    {
+      connection.Open();
+
+      string query = @"
+            SELECT *
+            FROM (
+                SELECT *,
+                       ROW_NUMBER() OVER (PARTITION BY category ORDER BY publishdate DESC) AS rn
+                FROM news
+                WHERE category IN ('gundem', 'spor', 'yasam', 'bilim')
+            ) AS subquery
+            WHERE rn = 1;";
+
+
+
+      using (var command = new NpgsqlCommand(query, connection))
+      {
+
+
+        using (var reader = command.ExecuteReader())
+        {
+          while (reader.Read())
+          {
+            var publishedAt = reader.GetDateTime(reader.GetOrdinal("publishdate"));
+
+            DateTime now = DateTime.Now;
+            TimeSpan timeDifference = now - publishedAt;
+
+            string timeAgo;
+            if (timeDifference.TotalMinutes < 60)
+            {
+              timeAgo = $"{(int)timeDifference.TotalMinutes} dakika önce";
+            }
+            else if (timeDifference.TotalHours < 24)
+            {
+              timeAgo = $"{(int)timeDifference.TotalHours} saat önce";
+            }
+            else
+            {
+              timeAgo = $"{(int)timeDifference.TotalDays} gün önce";
+            }
+
+
+            var cekilenhaber = new SonDakika
+            {
+              Title = reader.GetString(reader.GetOrdinal("title")),
+              ImageUrl = reader.IsDBNull(reader.GetOrdinal("image")) ? "/assets/img/briefxlogo.png" : reader.GetString(reader.GetOrdinal("image")),
+              Link = reader.GetString(reader.GetOrdinal("link")),
+              Publisher = reader.GetString(reader.GetOrdinal("publisher")),
+              PublishedAtFormatted = timeAgo
+            };
+
+            sondakikahaberleri.Add(cekilenhaber);
+          }
+        }
+      }
+    }
+  }
+
+  public class SonDakika
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string ImageUrl { get; set; }
+    public string Link { get; set; }
+    public string Publisher { get; set; }
+    public string PublishedAtFormatted { get; set; }
+    public string Category { get; set; }
+  }
+  public class Haber
+    {
+      public int Id { get; set; }
+      public string Title { get; set; }
+      public string ImageUrl { get; set; }
+      public string Link { get; set; }
+      public string Publisher { get; set; }
+      public string PublishedAtFormatted { get; set; }
+      public string Category { get; set; }
+  }
+
+}
+
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,316 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Npgsql;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using HtmlAgilityPack;
+
+namespace pulseui.Pages
+{
+  public class IndexModel : PageModel
+  {
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+      _logger = logger;
+    }
+
+    private string _connectionString;
+    public List<SonDakika> sondakikahaberleri { get; set; } = new List<SonDakika> { };
+    public List<Haber> news { get; set; } = new List<Haber> { };
+
+    public static string UsdRate { get; set; }
+    public static string EuroRate { get; set; }
+    public static string GramAltinRate { get; set; }
+    public static string BtcRate { get; set; }
+    public async Task OnGetAsync(string search = null)
+    {
+      BtcRate = await FetchBTCLastFieldValue();
+      GramAltinRate = await FetchLastFieldValue();
+      UsdRate = await GetUsdRateAsync();
+      EuroRate = await GetEuroRateAsync();
+      Sondakika();
+      _connectionString = "User ID=atedevuser1;Password=atedevpostgrepwd1;Server=10.150.0.79;Port=31821;Database=pulse;";
+
+      using (var connection = new NpgsqlConnection(_connectionString))
+      {
+        connection.Open();
+
+        string query = "SELECT * FROM news where category='gundem' ORDER BY publishdate DESC";
+        if (!string.IsNullOrEmpty(search))
+        {
+          query = "SELECT * FROM news where title ILIKE @search ORDER BY publishdate DESC";
+        }
+
+        using (var command = new NpgsqlCommand(query, connection))
+        {
+          if (!string.IsNullOrEmpty(search))
+          {
+            command.Parameters.AddWithValue("@search", "%" + search + "%");
+          }
+
+          using (var reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              var publishedAt = reader.GetDateTime(reader.GetOrdinal("publishdate"));
+
+              DateTime now = DateTime.Now;
+              TimeSpan timeDifference = now - publishedAt;
+
+              string timeAgo;
+              if (timeDifference.TotalMinutes < 60)
+              {
+                timeAgo = $"{(int)timeDifference.TotalMinutes} dakika önce";
+              }
+              else if (timeDifference.TotalHours < 24)
+              {
+                timeAgo = $"{(int)timeDifference.TotalHours} saat önce";
+              }
+              else
+              {
+                timeAgo = $"{(int)timeDifference.TotalDays} gün önce";
+              }
+
+              var cekilenhaber = new Haber
+              {
+                Title = reader.GetString(reader.GetOrdinal("title")),
+                ImageUrl = reader.IsDBNull(reader.GetOrdinal("image")) ? "/assets/img/briefxlogo.png" : reader.GetString(reader.GetOrdinal("image")),
+                Link = reader.GetString(reader.GetOrdinal("link")),
+                Publisher = reader.GetString(reader.GetOrdinal("publisher")),
+                PublishedAtFormatted = timeAgo
+              };
+
+              news.Add(cekilenhaber);
+            }
+          }
+        }
+      }
+    }
+
+    public void Sondakika()
+    {
+      _connectionString = "User ID=atedevuser1;Password=atedevpostgrepwd1;Server=10.150.0.79;Port=31821;Database=pulse;";
+
+      using (var connection = new NpgsqlConnection(_connectionString))
+      {
+        connection.Open();
+
+        string query = @"
+                    SELECT *
+                    FROM (
+                        SELECT *,
+                            ROW_NUMBER() OVER (PARTITION BY category ORDER BY publishdate DESC) AS rn
+                        FROM news
+                        WHERE category IN ('gundem', 'spor', 'yasam', 'bilim')
+                    ) AS subquery
+                    WHERE rn = 1;";
+
+        using (var command = new NpgsqlCommand(query, connection))
+        {
+          using (var reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              var publishedAt = reader.GetDateTime(reader.GetOrdinal("publishdate"));
+
+              DateTime now = DateTime.Now;
+              TimeSpan timeDifference = now - publishedAt;
+
+              string timeAgo;
+              if (timeDifference.TotalMinutes < 60)
+              {
+                timeAgo = $"{(int)timeDifference.TotalMinutes} dakika önce";
+              }
+              else if (timeDifference.TotalHours < 24)
+              {
+                timeAgo = $"{(int)timeDifference.TotalHours} saat önce";
+              }
+              else
+              {
+                timeAgo = $"{(int)timeDifference.TotalDays} gün önce";
+              }
+
+              var cekilenhaber = new SonDakika
+              {
+                Title = reader.GetString(reader.GetOrdinal("title")),
+                ImageUrl = reader.IsDBNull(reader.GetOrdinal("image")) ? "/assets/img/briefxlogo.png" : reader.GetString(reader.GetOrdinal("image")),
+                Link = reader.GetString(reader.GetOrdinal("link")),
+                Publisher = reader.GetString(reader.GetOrdinal("publisher")),
+                PublishedAtFormatted = timeAgo
+              };
+
+              sondakikahaberleri.Add(cekilenhaber);
+            }
+          }
+        }
+      }
+    }
+
+
+
+   
+      public async Task<string> FetchBTCLastFieldValue()
+    {
+
+      var url = " https://webservice.foreks.com/foreks-web-widget/qbOBC";
+      using (HttpClient client = new HttpClient())
+      {
+        var response = await client.GetAsync(url);
+        var pageContent = await response.Content.ReadAsStringAsync();
+        var htmlDoc = new HtmlDocument();
+
+        htmlDoc.LoadHtml(pageContent);
+
+        var lastFieldNode = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='lastField']");
+        if (lastFieldNode != null)
+        {
+          string originalValue= lastFieldNode.InnerText.Trim();
+          int decimalIndex = originalValue.IndexOf('.');
+
+          string result;
+          if (decimalIndex != -1)
+          {
+            result = originalValue.Substring(0, decimalIndex);
+          }
+          else
+          {
+            result = originalValue;
+          }
+          return result ;
+        }
+
+        return null;
+      }
+    }
+    public async Task<string> FetchLastFieldValue()
+    {
+
+      var url = "https://webservice.foreks.com/foreks-web-widget/RoyVJ";
+      using (HttpClient client = new HttpClient())
+      {
+        var response = await client.GetAsync(url);
+        var pageContent = await response.Content.ReadAsStringAsync();
+        var htmlDoc = new HtmlDocument();
+
+        htmlDoc.LoadHtml(pageContent);
+        
+        var lastFieldNode = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='lastField']");
+        if (lastFieldNode != null)
+        {
+          string originalValue =lastFieldNode.InnerText.Trim();
+          int decimalIndex = originalValue.IndexOf('.');
+
+          string result;
+          if (decimalIndex != -1)
+          {
+            result = originalValue.Substring(0, decimalIndex);
+          }
+          else
+          {
+            
+            result = originalValue;
+          }
+          return result;
+        }
+
+        return null;
+      }
+    }
+  
+
+
+
+
+  public async Task<string> GetEuroRateAsync()
+    {
+      var url = "https://www.tcmb.gov.tr/kurlar/today.xml";
+      using (HttpClient client = new HttpClient())
+      {
+        var response = await client.GetStringAsync(url);
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(response);
+
+        XmlNode usdNode = xmlDocument.SelectSingleNode("//Currency[@CurrencyCode='EUR']");
+        if (usdNode != null)
+        {
+          XmlNode forexBuyingNode = usdNode.SelectSingleNode("ForexBuying");
+          if (forexBuyingNode != null)
+          {
+            string originalValue = forexBuyingNode.InnerText;
+            int decimalIndex = originalValue.IndexOf('.');
+
+            if (decimalIndex != -1 && decimalIndex + 3 <= originalValue.Length)
+            {
+             
+              string result = originalValue.Substring(0, decimalIndex + 3);
+              return result;
+            }
+           
+
+
+          }
+        }
+        return "Değer bulunamadı";
+      }
+    }
+
+
+
+
+
+    public async Task<string> GetUsdRateAsync()
+    {
+      var url = "https://www.tcmb.gov.tr/kurlar/today.xml";
+      using (HttpClient client = new HttpClient())
+      {
+        var response = await client.GetStringAsync(url);
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(response);
+
+        XmlNode usdNode = xmlDocument.SelectSingleNode("//Currency[@CurrencyCode='USD']");
+        if (usdNode != null)
+        {
+          XmlNode forexBuyingNode = usdNode.SelectSingleNode("ForexBuying");
+          if (forexBuyingNode != null)
+          {
+            string originalValue = forexBuyingNode.InnerText;
+            int decimalIndex = originalValue.IndexOf('.');
+
+            if (decimalIndex != -1 && decimalIndex + 3 <= originalValue.Length)
+            {
+
+              string result = originalValue.Substring(0, decimalIndex + 3);
+              return result;
+            }
+          }
+        }
+        return "Değer bulunamadı";
+      }
+    }
+
+    public class SonDakika
+    {
+      public int Id { get; set; }
+      public string Title { get; set; }
+      public string ImageUrl { get; set; }
+      public string Link { get; set; }
+      public string Publisher { get; set; }
+      public string PublishedAtFormatted { get; set; }
+      public string Category { get; set; }
+    }
+
+    public class Haber
+    {
+      public int Id { get; set; }
+      public string Title { get; set; }
+      public string ImageUrl { get; set; }
+      public string Link { get; set; }
+      public string Publisher { get; set; }
+      public string PublishedAtFormatted { get; set; }
+      public string Category { get; set; }
+    }
+  }
+}
